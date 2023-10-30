d5d05d8cd1e1032c5dc46b960042f418af79285c
harshitphoenix <img_2019022@iiitm.ac.in>
2023-10-30 23:32:09 +0530

    add theme change buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,24 +2,35 @@ import next from "next";
 import SearchBar from "./SearchBar";
 import styles from "../styles/Header.module.css";
 import { FaMoon, FaSun } from "react-icons/fa";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 const Header = () => {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+    useEffect(() => {
+        const theme = document.body.style.getPropertyValue('--current-theme') as "light" | "dark";
+        setTheme(theme);
+    }, [])
+
   const changeTheme = () => {
     if (typeof window !== "undefined") {
       document.body.classList.toggle("dark");
+      setTheme(theme === "dark" ? "light" : "dark");
     }
   };
 
   return (
-    // <header>
-      <div className={styles.container}>
+    <div className={styles.container}>
+      <Link href={"/"}>
         <h1>Groww Stonks</h1>
-        <SearchBar />
-        {/* <button>Change Theme</button> */}
-        {}
+      </Link>
+      <SearchBar />
+      {theme === "dark" ? (
         <FaSun onClick={changeTheme} />
+      ) : (
         <FaMoon onClick={changeTheme} />
-      </div>
-    // </header>
+      )}
+    </div>
   );
 };
 
