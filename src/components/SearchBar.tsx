8c42a06319bedda374323e110b46520fe0f2e5f9
harshitphoenix <img_2019022@iiitm.ac.in>
2023-10-31 08:24:30 +0530

    add session storage for caching prev search results

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,22 +3,12 @@ import styles from "../styles/SearchBar.module.css";
 import { FaSearch } from "react-icons/fa";
 import { debounce } from "@/utils/debounceSearch";
 import { DataService, SearchSuggestion } from "@/services/dataService";
-const comp = [
-  "Microsoft",
-  "Apple",
-  "Amazon",
-  "Google",
-  "Facebook",
-  "Tesla",
-  "Netflix",
-  "Paypal",
-  "Adobe",
-];
+
 const SearchBar = () => {
   const [search, setSearch] = useState("");
   const suggestionRef = useRef<HTMLDivElement>(null);
   const searchBarRef = useRef<HTMLInputElement>(null);
-
+  const [showSuggestions, setShowSuggestions] = useState(false);
   const [suggestions, setSuggestions] = useState<SearchSuggestion[]>([]);
 
   const debounceSearch = debounce(() => {
@@ -28,30 +18,52 @@ const SearchBar = () => {
     });
   }, 300);
 
+  const fetchFromSessionStorage = () => {
+    const data = sessionStorage.getItem("searchSuggestions");
+    if (data) {
+      setShowSuggestions(true);
+      setSuggestions(JSON.parse(data));
+    }
+  };
+
   const handleSuggestionClick = (symbol: string) => {
+    const data = sessionStorage.getItem("searchSuggestions");
+    if (data) {
+      const parsedData = JSON.parse(data);
+      parsedData.push(symbol);
+      sessionStorage.setItem("searchSuggestions", JSON.stringify(parsedData));
+    } else {
+      sessionStorage.setItem("searchSuggestions", JSON.stringify([symbol]));
+    }
     setSuggestions([]);
+    setShowSuggestions(false);
   };
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+    setShowSuggestions(true);
     debounceSearch(e.target.value);
   };
-  console.log(searchBarRef?.current?.clientWidth);
+
   return (
     <div ref={searchBarRef} className={styles.container}>
       <FaSearch />
       <input
+        onFocus={fetchFromSessionStorage}
         value={search}
         onChange={handleSearch}
         type="text"
         placeholder="Search stock & etfs"
       />
-      {suggestions.length > 0 && (
+      {showSuggestions && (
         <div
           style={{ width: searchBarRef?.current?.clientWidth }}
           ref={suggestionRef}
           className={styles.suggestions}
         >
+          {suggestions.length === 0 && (
+            <div className={styles.suggestionItems}>No results found</div>
+          )}
           {suggestions.map((val, index) => (
             <div
               onClick={() => handleSuggestionClick(val.symbol)}
