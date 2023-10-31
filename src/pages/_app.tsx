d1a971ef4cc1d1b3f1e4528b1de3e998dfd374d3
harshitphoenix <img_2019022@iiitm.ac.in>
2023-10-31 07:09:48 +0530

    add react queery for caching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { store } from "@/redux/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
-
+const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </QueryClientProvider>
   );
 }
